3fc099bd34d01c87f2716247b86f5092d77d846d
MikitaSerou <MikitaSerou@gmail.com>
2021-06-08 13:03:12 +0300

    Init

---
+++ src/main/java/com/example/task4itr/Task4itrApplication.java
@@ -0,0 +1,13 @@
+package com.example.task4itr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Task4itrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Task4itrApplication.class, args);
+    }
+
+}
