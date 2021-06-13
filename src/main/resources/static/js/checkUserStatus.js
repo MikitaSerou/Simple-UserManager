96bfb2f521b561a51c51d5c3b6beb4dda98b1922
MikitaSerou <Mikita.serou@gmail.com>
2021-06-14 01:15:24 +0300

    jquery page reloading9

--- src/main/resources/static/js/checkUserStatus.js
+++ src/main/resources/static/js/checkUserStatus.js
@@ -2,7 +2,7 @@ $(window).bind('beforeunload', function(){
     let currStatus = document.getElementById('currentUserStatus').value;
     $(window).on('unload', function() {
         if(currStatus === 'false'){
-            window.location.href = '/logout';
+            window.location = "/logout";
         }
     });
 });
