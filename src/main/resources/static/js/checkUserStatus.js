13b7af8e75cdfccd4cd2db5213f10c6ffc89f0cc
MikitaSerou <Mikita.serou@gmail.com>
2021-06-14 00:58:09 +0300

    jquery page reloading5

--- src/main/resources/static/js/checkUserStatus.js
+++ src/main/resources/static/js/checkUserStatus.js
@@ -2,7 +2,6 @@ $(window).bind('beforeunload', function(){
     let currStatus = document.getElementById('currentUserStatus').value;
     console.log(currStatus)
     if(currStatus === 'false'){
-        console.log("User Blocked")
-        window.location = "/logout";
+        window.location = '/logout';
     }
 });
