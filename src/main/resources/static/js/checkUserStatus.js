b91f01390e615aaca6168f19d784b44aace35030
MikitaSerou <Mikita.serou@gmail.com>
2021-06-14 00:11:52 +0300

    Usr checking status script

--- src/main/resources/static/js/checkUserStatus.js
+++ src/main/resources/static/js/checkUserStatus.js
@@ -1,6 +1,7 @@
 $(document).ready ( function(){
     let currStatus = document.getElementById('currentUserStatus').value;
     if(!currStatus){
+        console.log("User Blocked")
         window.location = "/logout";
     }
 });
